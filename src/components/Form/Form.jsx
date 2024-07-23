f492df6cd1f05f736c1356c9cc2605646d1969c7
Riokoko <Riokoko2@gmail.com>
2024-07-24 02:05:00 +0300

    added OZU box

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,12 +1,19 @@
 import React, {useCallback, useEffect, useState, useRef} from 'react';
 import './Form.css';
 import {useTelegram} from "../../hooks/useTelegram";
-
+import { default as ReactSelect, components } from "react-select";
+import { OZU } from "./data.js";
 
 const Form = () => {
     const [processor, setProcessor] = useState('Intel');
     const {tg} = useTelegram();
 
+
+    const handleChange = (selected) => {
+        setState({
+          optionSelected: selected
+        });
+    };
     const onSendData = useCallback(() => {
         const data = {
             processor
@@ -40,34 +47,24 @@ const Form = () => {
         setProcessor(e.target.value)
     }
 
+    const Option = (props) => {
+        return (
+          <div>
+            <components.Option {...props}>
+              <input
+                type="checkbox"
+                checked={props.isSelected}
+                onChange={() => null}
+              />{" "}
+              <label>{props.label}</label>
+            </components.Option>
+          </div>
+        );
+      };
 
-    const showPlatformOptions =()=> {
-        //  let checkboxes = el;
-        //  console.log("ref:",myRef.current)
-        //   if (!expanded) {
-        //    //checkboxes.style.display = "block";
-        //     setExpanded(true);
-        //   } else {
-        //   // checkboxes.style.display = "none";
-        //     setExpanded(false);
-        //   }
+      const [state, setState] = useState({ optionSelected: null });
       
-      
-        }
 
-
-    const [query, setQuery] = useState("");
-    const [expanded,setExpanded] = useState(false);
-    const [selectionOptions, setSelectionOptions] = useState(["Instagram","LinkedIn","Twitter"]);
-    const myRef = useRef(null);
-    
-    
-    
-    const handleQueryChange = event => {
-        console.log("Event:",event.target.value)
-        setQuery(event.target.value);
-    };
-        
 
     return (
         <div className={"form"}>
@@ -79,40 +76,25 @@ const Form = () => {
                 <option value={'Intel+AMD'}>Intel+AMD</option>
             </select>
 
-            <select className="w-full font-semibold"  onChange={handleQueryChange}>
-              {selectionOptions.map((platform,x) => (
-                  <option key={x} value={platform}>
-                      {platform}
-                  </option>
-              ))}
-          </select>
+            <div>
+                <h3>Выбор оперативной памяти</h3>
+                <ReactSelect
+                    options={OZU}
+                    isMulti
+                    closeMenuOnSelect={false}
+                    hideSelectedOptions={false}
+                    components={{
+                    Option
+                    }}
+                    onChange={handleChange}
+                    value={state.optionSelected}
+                    // Hide dropdown list  when select any item
+                    // closeMenuOnSelect={true}
 
-
-          <div className="relative" onClick={showPlatformOptions}>
-          <h6>PLATFORMS </h6>
-          <select className="w-full font-semibold"  onChange={handleQueryChange}>
-              {selectionOptions.map((platform,x) => (
-                  <option key={x} value={platform}>
-                      {platform}
-                  </option>
-              ))}
-          </select>
-          <div className="absolute left-0 right-0 top-0 bottom-0"></div>
-        </div>
-        <div 
-        ref={myRef}
-        className="checkboxes border-gray-200 border border-solid">
-          <label htmlFor="one" className="block ">
-            <input type="checkbox" id="one" onChange={handleQueryChange} className="m-3"/>
-            Instagram</label>
-          <label htmlFor="two" className="block">
-            <input type="checkbox" id="two" onChange={handleQueryChange} className="m-3"/>
-            LinkedIn</label>
-          <label htmlFor="three" className="block">
-            <input type="checkbox" id="three" onChange={handleQueryChange} className="m-3"/>
-            Twitter</label>
-        </div>
-
+                    //Selected Item Remove in dropdown list
+                    // hideSelectedOptions={true}
+                />
+            </div>
         </div>
 
 
---
+++ src/components/Form/data.js
@@ -0,0 +1,22 @@
+export const OZU = [
+    { value: "1", label: "1" },
+    { value: "1.5", label: "1.5" },
+    { value: "2", label: "2" },
+    { value: "3", label: "3" },
+    { value: "4", label: "4" },
+    { value: "5", label: "5" },
+    { value: "6", label: "6" },
+    { value: "8", label: "8" },
+    { value: "12", label: "12" },
+    { value: "16", label: "16" },
+    { value: "18", label: "18" },
+    { value: "20", label: "20" },
+    { value: "24", label: "24" },
+    { value: "32", label: "32" },
+    { value: "40", label: "40" },
+    { value: "48", label: "48" },
+    { value: "64", label: "64" },
+    { value: "96", label: "96" },
+    { value: "128", label: "128" },
+    { value: "Отсутствует", label: "Отсутствует" }
+  ];
