7dd6f28df3f9386838f1baadbcb49c75e5d17294
Riokoko <Riokoko2@gmail.com>
2024-07-24 01:45:50 +0300

    added multi-select checkbox

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,6 +1,7 @@
-import React, {useCallback, useEffect, useState} from 'react';
+import React, {useCallback, useEffect, useState, useRef} from 'react';
 import './Form.css';
 import {useTelegram} from "../../hooks/useTelegram";
+
 
 const Form = () => {
     const [processor, setProcessor] = useState('Intel');
@@ -19,7 +20,7 @@ const Form = () => {
             tg.offEvent('mainButtonClicked', onSendData)
         }
     }, [onSendData])
-
+    
     useEffect(() => {
         tg.MainButton.setParams({
             text: 'Отправить данные'
@@ -39,6 +40,35 @@ const Form = () => {
         setProcessor(e.target.value)
     }
 
+
+    const showPlatformOptions =()=> {
+        //  let checkboxes = el;
+        //  console.log("ref:",myRef.current)
+        //   if (!expanded) {
+        //    //checkboxes.style.display = "block";
+        //     setExpanded(true);
+        //   } else {
+        //   // checkboxes.style.display = "none";
+        //     setExpanded(false);
+        //   }
+      
+      
+        }
+
+
+    const [query, setQuery] = useState("");
+    const [expanded,setExpanded] = useState(false);
+    const [selectionOptions, setSelectionOptions] = useState(["Instagram","LinkedIn","Twitter"]);
+    const myRef = useRef(null);
+    
+    
+    
+    const handleQueryChange = event => {
+        console.log("Event:",event.target.value)
+        setQuery(event.target.value);
+    };
+        
+
     return (
         <div className={"form"}>
             <h3>Введите параметры ноутбука</h3>
@@ -48,7 +78,45 @@ const Form = () => {
                 <option value={'AMD'}>AMD</option>
                 <option value={'Intel+AMD'}>Intel+AMD</option>
             </select>
+
+            <select className="w-full font-semibold"  onChange={handleQueryChange}>
+              {selectionOptions.map((platform,x) => (
+                  <option key={x} value={platform}>
+                      {platform}
+                  </option>
+              ))}
+          </select>
+
+
+          <div className="relative" onClick={showPlatformOptions}>
+          <h6>PLATFORMS </h6>
+          <select className="w-full font-semibold"  onChange={handleQueryChange}>
+              {selectionOptions.map((platform,x) => (
+                  <option key={x} value={platform}>
+                      {platform}
+                  </option>
+              ))}
+          </select>
+          <div className="absolute left-0 right-0 top-0 bottom-0"></div>
         </div>
+        <div 
+        ref={myRef}
+        className="checkboxes border-gray-200 border border-solid">
+          <label htmlFor="one" className="block ">
+            <input type="checkbox" id="one" onChange={handleQueryChange} className="m-3"/>
+            Instagram</label>
+          <label htmlFor="two" className="block">
+            <input type="checkbox" id="two" onChange={handleQueryChange} className="m-3"/>
+            LinkedIn</label>
+          <label htmlFor="three" className="block">
+            <input type="checkbox" id="three" onChange={handleQueryChange} className="m-3"/>
+            Twitter</label>
+        </div>
+
+        </div>
+
+
+
     );
 };
 
